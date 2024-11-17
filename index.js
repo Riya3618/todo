f5c8abbefe54f951a4d9f3aa075148b4e7649ecc
Riya3618 <riyajhagogo@gmail.com>
2024-11-17 18:13:34 +0530

    Cleaned up code

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-//dependencies required for the app
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-//render css files
+
 app.use(express.static("public"));
 
-//placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
-//placeholders for removed task
-var complete = ["finish jquery"];
+var task = [];
 
-//post route for adding new task 
+var complete = [];
+
 app.post("/addtask", function(req, res) {
     var newTask = req.body.newtask;
-    //add the new task from the post route
     task.push(newTask);
     res.redirect("/");
 });
 
 app.post("/removetask", function(req, res) {
     var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
     if (typeof completeTask === "string") {
         complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
         task.splice(task.indexOf(completeTask), 1);
     } else if (typeof completeTask === "object") {
         for (var i = 0; i < completeTask.length; i++) {
